ac4bba844f629a72188605217a2cb9820ce2f853
Rodrigo Costa <54584945+opoderosojeffao@users.noreply.github.com>
2024-05-24 15:50:53 -0400

    Adding Delete and Edit contacts API endpoints

---
+++ api/DeleteContact.php
@@ -0,0 +1,63 @@
+<?php
+/* Delete Contact Endpoint
+ * Small Project
+ * Team 3 - COP4331
+ * Dr. Leinecker
+ * Summer 2034 - 5/23/2024 */
+	$inData = getRequestInfo();
+
+    /* NOTE FOR SELF -> CHANGE THE DB CONNECTION INFO (Create user and password)
+                     -> CHANGE THE NAME OF THE PARAMETERS FROM FRONT-END*/
+	
+    // Variable holding parameters that will be deleted
+	$id = $inData['id'];
+	//$lastName = $inData['lastName'];
+    //$phoneNumber = $inData['phoneNumber'];
+	//$email = $inData['email'];
+
+    // Variable parameters to connect to the DB
+    $address = "localhost";
+    $user = "";
+    $password = "";
+    $database_name = "utopia";
+
+    // Connecting to the Database
+	$conn = new mysqli($address, $user, $password, $database_name);
+
+    // Trying connection
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        // Statement to delete the parameters
+		$stmt = $conn->prepare("DELETE FROM Contacts where ID=?");
+		$stmt->bind_param("i", $id); // Biding the ?s and expecting the ss to make it harder to have SQL injections
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+    // Function that request JSON info and converts it into a PHP value
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    // Send the value obj from a PHP value to JSON and prints it to the screen
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+    // Return the error
+    function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
---
+++ api/EditContact.php
@@ -0,0 +1,67 @@
+<?php
+/* Edit Contact Endpoint
+ * Small Project
+ * Team 3 - COP4331
+ * Dr. Leinecker
+ * Summer 2034 - 5/22/2024 */
+	$inData = getRequestInfo();
+
+    /* NOTE FOR SELF -> CHANGE THE DB CONNECTION INFO (Create user and password)
+                     -> CHANGE THE NAME OF THE PARAMETERS FROM FRONT-END */
+	
+    // Getting the ID of the contact that wil be edited
+    $id = $inData['id'];
+	$firstName = $inData['firstName'];
+	$lastName = $inData["lastName"];
+    $phoneNumber = $inData["phoneNumber"];
+	$email = $inData["email"];
+
+
+
+    // Variable parameters to connect to the DB
+    $address = "localhost";
+    $user = "";
+    $password = "";
+    $database_name = "utopia";
+
+    // Connecting to the Database
+	$conn = new mysqli($address, $user, $password, $database_name);
+
+    // Trying connection
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        // Statement to edit the parameters
+		$stmt = $conn->prepare("UPDATE contacts SET name_first=?, name_last=?, phone=?, email=? WHERE id=?");
+		// This makes the connection safer (avoid injection attacks)
+		$stmt->bind_param("ssssi", $firstName, $lastName, $email, $phoneNumber, $id); 
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+    // Function that request JSON info and converts it into a PHP value
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    // Send the value obj from a PHP value to JSON and prints it to the screen
+    function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+    // Return the error
+    function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
