98b75cea6c1bd7e801e9d458f82cb48ec0610fd3
JJCUBER <jehjeh1020@gmail.com>
2024-05-29 20:15:24 -0400

    Partially set up login and signup buttons

--- ui/src/scripts/Account.ts
+++ ui/src/scripts/Account.ts
@@ -1,8 +1,18 @@
 import $ from "jquery";
+import { request } from "./API";
 
 $(() => {
-    let loginForm = $<HTMLDivElement>("#loginForm");
-    let signupForm = $<HTMLDivElement>("#signupForm");
+    const loginForm = $<HTMLDivElement>("#loginForm");
+    const signupForm = $<HTMLDivElement>("#signupForm");
+
+    const loginUsernameInput = $<HTMLInputElement>("#loginUsernameInput");
+    const loginPasswordInput = $<HTMLInputElement>("#loginPasswordInput");
+    const signupUsernameInput = $<HTMLInputElement>("#signupUsernameInput");
+    const signupPasswordInput = $<HTMLInputElement>("#signupPasswordInput");
+    const signupFirstNameInput = $<HTMLInputElement>("#signupFirstNameInput");
+    const signupLastNameInput = $<HTMLInputElement>("#signupLastNameInput");
+
+    // handle swapping between logging in and signing up
     $("#loginTabButton").on("click", () => {
         loginForm.removeClass("inactive");
         signupForm.addClass("inactive");
@@ -10,5 +20,33 @@ $(() => {
     $("#signupTabButton").on("click", () => {
         loginForm.addClass("inactive");
         signupForm.removeClass("inactive");
+    });
+
+    $("#loginButton").on("click", () => {
+        request("Login",
+            {
+                username: loginUsernameInput.val(),
+                password: loginPasswordInput.val(),
+            },
+            (response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
+    });
+
+    $("#signUpButton").on("click", () => {
+        request("RegisterUser",
+            {
+                username: signupUsernameInput.val(),
+                password: signupPasswordInput.val(),
+                name_first: signupFirstNameInput.val(),
+                name_last: signupLastNameInput.val(),
+            },
+            (response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
     });
 });
