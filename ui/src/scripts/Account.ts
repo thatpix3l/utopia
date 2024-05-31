84e17781bf1dc0602ffb5209c02d6de9e7a1e933
JJCUBER <jehjeh1020@gmail.com>
2024-05-30 22:34:01 -0400

    Move cookie-related stuff to separate file

--- ui/src/scripts/Account.ts
+++ ui/src/scripts/Account.ts
@@ -63,32 +63,3 @@ $(() => {
             });
     });
 });
-
-function storeUserToCookie(user: User) {
-    const offset = 20 * 60 * 1000;
-    // toGMTString() is a (deprecated) alias of toUTCString(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
-    document.cookie = `id=${user.id},firstName=${user.firstName},lastName=${user.lastName};expires=${(new Date(Date.now() + offset)).toUTCString()}`;
-}
-
-function getUserFromCookie() {
-    const user: User = { id: 0, firstName: "", lastName: "" };
-
-    document.cookie.split(",").map((expression) => {
-        const [key, value] = expression.split("=");
-        switch (key) {
-            case "id":
-                user.id = parseInt(value);
-                break;
-            case "firstName":
-                user.firstName = value;
-                break;
-            case "lastName":
-                user.lastName = value;
-                break;
-        }
-    })
-
-    return user;
-}
-
-// window.location.href = "/";
