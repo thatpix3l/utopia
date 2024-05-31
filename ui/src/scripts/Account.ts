b498e1446268b1fc9360b6f5ac9c4b7eb828dcba
JJCUBER <jehjeh1020@gmail.com>
2024-05-30 22:11:46 -0400

    Set up storing to and loading from cookies

--- ui/src/scripts/Account.ts
+++ ui/src/scripts/Account.ts
@@ -2,8 +2,9 @@ import $ from "jquery";
 
 import { request } from "./API";
 
-let userID = -1;
 $(() => {
+    let userID = -1;
+
     const loginForm = $<HTMLDivElement>("#loginForm");
     const signupForm = $<HTMLDivElement>("#signupForm");
 
@@ -60,3 +61,38 @@ $(() => {
             });
     });
 });
+
+type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+}
+
+function storeUserToCookie(user: User) {
+    const offset = 20 * 60 * 1000;
+    // toGMTString() is a (deprecated) alias of toUTCString(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
+    document.cookie = `id=${user.id},firstName=${user.firstName},lastName=${user.lastName};expires=${(new Date(Date.now() + offset)).toUTCString()}`;
+}
+
+function getUserFromCookie() {
+    const user: User = { id: 0, firstName: "", lastName: "" };
+
+    document.cookie.split(",").map((expression) => {
+        const [key, value] = expression.split("=");
+        switch (key) {
+            case "id":
+                user.id = parseInt(value);
+                break;
+            case "firstName":
+                user.firstName = value;
+                break;
+            case "lastName":
+                user.lastName = value;
+                break;
+        }
+    })
+
+    return user;
+}
+
+// window.location.href = "/";
