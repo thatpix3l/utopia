8e6e480d9d03e170d1cc70993116c7e34658d8c2
JJCUBER <jehjeh1020@gmail.com>
2024-06-03 20:11:28 -0400

    add icons to add/delete buttons

--- ui/src/scripts/Contacts.ts
+++ ui/src/scripts/Contacts.ts
@@ -62,9 +62,9 @@ $(() => {
 
             let actions = document.createElement("div");
             let editButton = document.createElement("button");
-            editButton.innerText = "Edit";
+            editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75z"/></svg>`;
             let deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
+            deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"/></svg>`;
             $(deleteButton).on("click", () => {
                 if (confirm(`Warning, this will delete the contact with the name '${contact.firstName} ${contact.lastName}'.`)) {
                     request("DeleteContact",
